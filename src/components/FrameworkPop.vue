7aada983b0b973245483cc295f71bca00a1ab01f
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-13 02:01:06 +0700

    Update 7

---
+++ src/components/FrameworkPop.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+<template>
+  <v-card class="mx-auto" max-width="344" variant="outlined">
+    <v-img
+      src="https://img.icons8.com/color/512/vue-js.png"
+      height="200px"
+    ></v-img>
+    <v-card-item>
+      <div class="text-center">
+        <div class="text-h6 mb-1">Vue Js</div>
+        <div class="text-caption">Score : 100</div>
+      </div>
+    </v-card-item>
+
+    <v-card-actions class="justify-center">
+      <v-btn variant="outlined"> - </v-btn>
+      <v-btn variant="outlined"> + </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
--- src/views/StarredView.vue
+++ src/views/StarredView.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FrameworkPop from "@/components/FrameworkPop.vue";
+</script>
 
 <template>
-  <div>Starred</div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <FrameworkPop></FrameworkPop>
+      </v-col>
+      <v-col>
+        <FrameworkPop></FrameworkPop>
+      </v-col>
+      <v-col>
+        <FrameworkPop></FrameworkPop>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
