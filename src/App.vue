73fb62f59fd0a15f36adc24a208655851f9109bd
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-13 00:13:06 +0700

    Update 1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :prepend-icon="theme === 'light' ? mdiWeatherSunny : mdiWeatherNight"
+        @click="onClick"
+        >Toggle Theme</v-btn
+      >
+    </v-app-bar>
+
+    <v-main>
+      <v-container>Content area</v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { mdiWeatherSunny, mdiWeatherNight } from "@mdi/js";
+const theme = ref("light");
+
+function onClick() {
+  theme.value = theme.value === "light" ? "dark" : "light";
+}
+</script>
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+const app = createApp(App);
+app.use(vuetify);
+app.use(router);
+
+app.mount("#app");
