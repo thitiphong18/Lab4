116ee6bfddb7a97bd403fb33393754571372afe6
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-13 00:26:30 +0700

    Update 2

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,28 @@
     </v-app-bar>
 
     <v-main>
-      <v-container>Content area</v-container>
+      <v-container>
+        <v-icon :icon="mdiVuetify"></v-icon>
+        <v-icon :icon="mdiCursorPointer"></v-icon>
+        <v-btn class="ma-2" color="primary">
+          Accept
+          <v-icon end :icon="mdiCheckboxMarkedCircle"></v-icon>
+        </v-btn>
+        Content area</v-container
+      >
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { mdiWeatherSunny, mdiWeatherNight } from "@mdi/js";
+import {
+  mdiWeatherSunny,
+  mdiWeatherNight,
+  mdiCursorPointer,
+  mdiVuetify,
+  mdiCheckboxMarkedCircle,
+} from "@mdi/js";
 const theme = ref("light");
 
 function onClick() {
