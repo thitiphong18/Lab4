2ff2a1a4a9379d81c4451a4e0674765dc1e8167e
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-13 20:30:37 +0700

    Update 17

--- src/views/MyFilesView.vue
+++ src/views/MyFilesView.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FancyButton from "@/components/FancyButton.vue";
+import { mdiHeartOutline } from "@mdi/js";
+</script>
 
 <template>
-  <div>My Files</div>
-  <RouterLink to="/starred">Starred</RouterLink>
-  <v-btn to="/starred">Starred</v-btn>
+  <v-container>
+    <v-row>
+      <v-col
+        ><FancyButton><v-icon :icon="mdiHeartOutline" /> Click Me</FancyButton>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
