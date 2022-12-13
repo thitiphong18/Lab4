509893038d741f87bc17e28536ae23481ab770d6
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-13 16:49:23 +0700

    Update 10

--- src/views/StarredView.vue
+++ src/views/StarredView.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
 import FrameworkPop from "@/components/FrameworkPop.vue";
+const frameList = [
+  {
+    name: "VueJS",
+    img: "https://img.icons8.com/color/512/vue-js.png",
+    rating: 10,
+  },
+  {
+    name: "AngularJS",
+    img: "https://material.angularjs.org/latest/img/logo.svg",
+    rating: 20,
+  },
+  {
+    name: "ReatJS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+    rating: 14,
+  },
+];
 </script>
 
 <template>
   <v-container>
     <v-row>
-      <v-col>
+      <v-col v-for="(item, index) of frameList" :key="index">
         <FrameworkPop
-          name="VueJS"
-          img="https://img.icons8.com/color/512/vue-js.png"
-          :rating="10"
+          :name="item.name"
+          :img="item.img"
+          :rating="item.rating"
         ></FrameworkPop>
       </v-col>
     </v-row>
