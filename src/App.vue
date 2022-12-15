afd241405d6df8b8251ca6b34e368331e966c144
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-15 23:37:13 +0700

    Update 20

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@
     <v-navigation-drawer expand-on-hover rail permanent>
       <v-list>
         <v-list-item
-          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-          title="Kew"
-          subtitle="sandra_a88@gmailcom"
+          prepend-avatar="https://scontent.fbkk2-4.fna.fbcdn.net/v/t39.30808-6/314609315_10225456965549690_5287212767421642758_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEU6zZYocwmxoP-nxC5Sh1EJaIkumxu-xIloiS6bG77EtkrneAOFqD7Dh6xbXRc1XY&_nc_ohc=g5MaLRt3dp0AX9dtnpB&_nc_zt=23&_nc_ht=scontent.fbkk2-4.fna&oh=00_AfAdl-RgnNuunV6u4l01MiTbzhm7pnF7ghC1etzbhaQCUA&oe=639F8587"
+          title="Thitiphong Nimmak"
+          subtitle="64160048@go.buu.ac.th"
         ></v-list-item>
       </v-list>
 
--- src/views/ShareWithMe.vue
+++ src/views/ShareWithMe.vue
@@ -1,5 +1,112 @@
 <script setup lang="ts"></script>
-
+<script>
+export default {
+  data: () => ({
+    colors: ["green", "purple", "orange", "indigo", "red"],
+    rating: 4.5,
+  }),
+};
+</script>
 <template>
-  <div>My Profile</div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card max-width="1400" class="mx-auto">
+          <v-layout>
+            <v-app-bar color="info" density="prominent">
+              <v-app-bar-title><h1>Portfolio</h1></v-app-bar-title>
+
+              <template v-slot:append> </template>
+            </v-app-bar>
+
+            <v-main>
+              <v-container fluid>
+                <v-card
+                  border
+                  class="mb-2"
+                  density="compact"
+                  prepend-avatar="https://scontent.fbkk2-4.fna.fbcdn.net/v/t39.30808-6/314609315_10225456965549690_5287212767421642758_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEU6zZYocwmxoP-nxC5Sh1EJaIkumxu-xIloiS6bG77EtkrneAOFqD7Dh6xbXRc1XY&_nc_ohc=g5MaLRt3dp0AX9dtnpB&_nc_zt=23&_nc_ht=scontent.fbkk2-4.fna&oh=00_AfAdl-RgnNuunV6u4l01MiTbzhm7pnF7ghC1etzbhaQCUA&oe=639F8587"
+                  subtitle="ฐิติพงศ์ นิ่มมาก"
+                  title="คิวเอง"
+                  variant="text"
+                >
+                  <v-img
+                    src="https://scontent.fbkk2-4.fna.fbcdn.net/v/t39.30808-6/314609315_10225456965549690_5287212767421642758_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEU6zZYocwmxoP-nxC5Sh1EJaIkumxu-xIloiS6bG77EtkrneAOFqD7Dh6xbXRc1XY&_nc_ohc=g5MaLRt3dp0AX9dtnpB&_nc_zt=23&_nc_ht=scontent.fbkk2-4.fna&oh=00_AfAdl-RgnNuunV6u4l01MiTbzhm7pnF7ghC1etzbhaQCUA&oe=639F8587"
+                    style="height: 70%"
+                    height="900"
+                    cover
+                  ></v-img>
+
+                  <v-card-text>
+                    <h2>ประวัติส่วนตัว</h2>
+
+                    สวัสดีครับ ผมนาย ฐิติพงศ์ นิ่มมาก ชื่อเล่น คิว เกิดวันที่ 17
+                    กรกฎาคม พ.ศ.2545 เกิดที่ กรุงเทพมหานคร สิ่งที่ชอบ เธอ
+                  </v-card-text>
+
+                  <template v-slot:actions>
+                    <v-btn color="primary" variant="text">View More</v-btn>
+
+                    <v-btn color="primary" variant="text">See in Map</v-btn>
+                  </template>
+                </v-card>
+
+                <v-card
+                  border
+                  density="comfortable"
+                  prepend-avatar="https://scontent.fbkk2-4.fna.fbcdn.net/v/t39.30808-6/314609315_10225456965549690_5287212767421642758_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEU6zZYocwmxoP-nxC5Sh1EJaIkumxu-xIloiS6bG77EtkrneAOFqD7Dh6xbXRc1XY&_nc_ohc=g5MaLRt3dp0AX9dtnpB&_nc_zt=23&_nc_ht=scontent.fbkk2-4.fna&oh=00_AfAdl-RgnNuunV6u4l01MiTbzhm7pnF7ghC1etzbhaQCUA&oe=639F8587"
+                  subtitle="ฐิติพงศ์ นิ่มมาก"
+                  title="คิวเอง"
+                  variant="text"
+                >
+                  <v-img
+                    src="https://www.matichon.co.th/wp-content/uploads/2017/06/93-1-728x450.jpg"
+                    height="800"
+                    cover
+                  ></v-img>
+
+                  <v-card-text>
+                    <h2>ประวัติการศึกษา</h2>
+
+                    ชั้นประธมศึกษา ศึกษาที่โรงเรียน อนุบาลวัดคลองใหญ่ ตำบล
+                    คลองใหญ่ อำเภอ คลองใหญ่ จังหวัด ตราด
+                    ชั้นมัธยยมศึกษาทั้งตอนต้นและต้อนปลาย ศึกษาที่โรงเรียน
+                    ตราษตระการคุณ ตำบล วังกระแจะ อำเภอ เมือง จังหวัด ตราด
+                  </v-card-text>
+                </v-card>
+
+                <v-card
+                  border
+                  density="comfortable"
+                  prepend-avatar="https://scontent.fbkk2-4.fna.fbcdn.net/v/t39.30808-6/314609315_10225456965549690_5287212767421642758_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEU6zZYocwmxoP-nxC5Sh1EJaIkumxu-xIloiS6bG77EtkrneAOFqD7Dh6xbXRc1XY&_nc_ohc=g5MaLRt3dp0AX9dtnpB&_nc_zt=23&_nc_ht=scontent.fbkk2-4.fna&oh=00_AfAdl-RgnNuunV6u4l01MiTbzhm7pnF7ghC1etzbhaQCUA&oe=639F8587"
+                  subtitle="ฐิติพงศ์ นิ่มมาก"
+                  title="คิวเอง"
+                  variant="text"
+                >
+                  <v-img
+                    src="https://www.matichon.co.th/wp-content/uploads/2017/06/93-1-728x450.jpg"
+                    height="800"
+                    cover
+                  ></v-img>
+
+                  <v-card-text>
+                    <h2>ประวัติการศึกษา</h2>
+
+                    ชั้นประธมศึกษา ศึกษาที่โรงเรียน อนุบาลวัดคลองใหญ่ ตำบล
+                    คลองใหญ่ อำเภอ คลองใหญ่ จังหวัด ตราด
+                    ชั้นมัธยยมศึกษาทั้งตอนต้นและต้อนปลาย ศึกษาที่โรงเรียน
+                    ตราษตระการคุณ ตำบล วังกระแจะ อำเภอ เมือง จังหวัด ตราด
+                  </v-card-text>
+                </v-card>
+              </v-container>
+            </v-main>
+          </v-layout>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col> </v-col>
+    </v-row>
+  </v-container>
 </template>
